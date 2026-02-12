a41d5e6f660efd087619fa87b31c87065ea4d15d
gizzard1 <mauriciodds86@gmail.com>
2026-02-12 15:59:53 -0600

    Updated loadTransaction to include transactions key to info returned, this will be use on mobile app

--- app/Http/Controllers/DataTransactions.php
+++ app/Http/Controllers/DataTransactions.php
@@ -8,6 +8,7 @@ use App\Models\cita;
 use App\Models\Entrada;
 use App\Models\Material;
 use App\Models\venta;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 
@@ -22,6 +23,7 @@ class DataTransactions extends Controller
             $cortes = [];
             $usos = [];
             $entradas = [];
+            $transactions = [];
             $salon_id = $request->user()->salon_id;
             $filters = $request->input('filters', [
                 'ventasFilter' => true,
@@ -33,29 +35,37 @@ class DataTransactions extends Controller
                 'dateFilter' => 'today',
             ]);
             $filter_type = $filters['dateFilter'];
-            if($filter_type === 'custom'){
+            if ($filter_type === 'custom') {
                 $start = $request->input('start_date');
+                $start = Carbon::parse($start)->startOfDay();
                 $end = $request->input('end_date');
+                $end = Carbon::parse($end)->endOfDay();
             } else {
                 [$start, $end] = self::getDateRange($filter_type);
             }
             if (self::castBoolean($filters['ventasFilter'])) {
                 $ventas = venta::transactionsBetweenDates($salon_id, $start, $end);
+                $transactions = $ventas;
             }
             if (self::castBoolean($filters['citasFilter'])) {
                 $citas = cita::transactionsBetweenDates($salon_id, $start, $end);
+                $transactions = $citas;
             }
             if (self::castBoolean($filters['aperturasFilter'])) {
                 $aperturas = caja_apertura::transactionsBetweenDates($salon_id, $start, $end);
+                $transactions = $aperturas;
             }
             if (self::castBoolean($filters['cortesFilter'])) {
                 $cortes = caja_corte::transactionsBetweenDates($salon_id, $start, $end);
+                $transactions = $cortes;
             }
             if (self::castBoolean($filters['usosFilter'])) {
                 $usos = Material::transactionsBetweenDates($salon_id, $start, $end);
+                $transactions = $usos;
             }
             if (self::castBoolean($filters['entradasFilter'])) {
                 $entradas = Entrada::transactionsBetweenDates($salon_id, $start, $end);
+                $transactions = $entradas;
             }
 
             $info = [
@@ -65,7 +75,10 @@ class DataTransactions extends Controller
                 'sales' => $ventas,
                 'entries' => $entradas,
                 'materials' => $usos,
+                'transactions' => $transactions,
             ];
+
+            Log::info('Transactions loaded successfully', ['salon_id' => $salon_id, 'filters' => $filters,'info' => $info['transactions']]);
 
             return response()->json([$info]);
         } catch (\Throwable $th) {
