e8dbe072de15c4c4784c425723650c26b8fc053c
gizzard1 <mauriciodds86@gmail.com>
2026-02-20 15:14:15 -0600

    Updated customer validation

--- resources/views/livewire/informes/transacciones/movimiento.blade.php
+++ resources/views/livewire/informes/transacciones/movimiento.blade.php
@@ -9,7 +9,7 @@
 </div>
 <div class="p-3 text-center">
     <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-user"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0" /><path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" /></svg>
-    <label><a href="{{ route('clientes', ['search' => $itemSelected->customer->first_name,'custId'=>$itemSelected->customer_id]) }}">{{ $itemSelected->customer->first_name }} {{ $itemSelected->customer->last_name }}</a></label>
+    <label><a href="{{ route('clientes', ['search' => $itemSelected->customer?->first_name,'custId'=>$itemSelected->customer_id]) }}">{{ $itemSelected->customer?->first_name }} {{ $itemSelected->customer?->last_name }}</a></label>
 </div>
 
 <div class="table-responsive">
