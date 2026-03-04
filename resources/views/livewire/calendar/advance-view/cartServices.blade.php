ada0f5f6470f596a3379760399d410b975ee85f2
gizzard1 <mauriciodds86@gmail.com>
2026-03-04 17:56:44 -0600

    Updated input and select fields's width to avoid horizontal scrolling

---
+++ resources/views/livewire/calendar/advance-view/cartServices.blade.php
@@ -0,0 +1,61 @@
+
+<tr>
+    <td class="service-title">{{ $item['name'] }}</td>
+    <td>
+        <input list="times-start" name="time-start" type="time" value="{{ $item['start'] }}" class="time-input" wire:change.prevent="$emit('changeStartDuration','{{ $item['id'] }}', $event.target.value)">
+        <datalist id="times-start">
+            @foreach($times as $time)
+                <option value="{{ $time }}">{{ $time }}</option>
+            @endforeach
+        </datalist>
+    </td>
+    <td>
+        <input list="times-end" name="time-end" type="time" value="{{ $item['end'] }}" class="time-input" wire:change.prevent="$emit('changeEndDuration','{{ $item['id'] }}', $event.target.value)">
+        <datalist id="times-end">
+            @foreach($times as $time)
+                <option value="{{ $time }}">{{ $time }}</option>
+            @endforeach
+        </datalist>
+    </td>
+    <td>
+        <select wire:change.prevent="$emit('changeEmpleado','servicio','{{ $item['id'] }}', $event.target.value)" class="service-title employees-title employee-selector">
+            @if(!isset($item['vendedor']))
+                <option value="">Seleccione un empleado</option>
+            @endif
+            @foreach($empleados as $empleado)
+                <option value="{{ $empleado->id }}" {{ $item['vendedor'] == $empleado->id ? 'selected' : '' }}>
+                    {{ $empleado->first_name }}
+                </option>
+            @endforeach
+        </select>
+    </td>
+    <td class="input-container d-flex">
+        <input
+            wire:change="$emit('updatePercentage','servicio', '{{ $item['id'] }}', $event.target.value)"
+            class="form-control text-center qty discount-input"
+            value="{{ isset($item['disccount_percent']) ? $item['disccount_percent'] : '' }}">
+        <!-- Select para elegir porcentaje o moneda, con flecha oculta -->
+        <select class="form-control no-arrow bg-white qty discount select-discount" wire:change="updatePercentageType('servicio','{{ $item['id'] }}',$event.target.value)">
+            <option value="Porcentaje" {{ $item['discount_type'] == "Porcentaje" ? 'selected' : '' }}>%</option>
+            <option value="Cantidad" {{ $item['discount_type'] == "Cantidad" ? 'selected' : '' }}>$</option>
+        </select>
+    </td>
+    <td>
+        <div class="d-flex prices">
+            <input
+                wire:change="$emit('changeTotalCP','servicio', '{{ $item['id'] }}', $event.target.value)"
+                class="form-control text-center qty" value="${{ $item['disccount_price'] ? $item['disccount_price'] : $item['sale_price'] }}">
+            <input type="radio" {{ $item['base_comision'] ?  '' : 'checked' }} name="{{ $item['id'] }}" wire:click="$emit('updateBaseComision','servicio','{{ $item['id'] }}',0)">
+        </div>
+    </td>
+    <td>
+        <div class="d-flex prices">
+            ${{ $item['total'] }}
+            <input type="radio"{{ $item['base_comision'] ?  'checked' : '' }} name="{{ $item['id'] }}" wire:click="$emit('updateBaseComision','servicio','{{ $item['id'] }}',1)" >
+        </div>
+    </td>
+    <td>
+        <button wire:click.prevent="$emit('deleteItem', '{{ $item['id'] }}','servicio',{{ $item['sid'] }})"
+            class="btn tp-btn btn-xxs btn-danger "><i class="fa fa-trash fa-lg"></i></button>
+    </td>
+</tr>
---
+++ resources/views/livewire/calendar/cart-view-styles.blade.php
@@ -0,0 +1,232 @@
+<style>
+    .swal2-textarea {
+        background-color: transparent;
+        color: black;
+    }
+
+    .col-lg-4,
+    .col-lg-8 {
+        padding-left: 0;
+        padding-right: 0;
+    }
+
+    input[name="time-start"],
+    input[name="time-end"] {
+        background-color: transparent;
+        border-color: transparent;
+        text-align: center;
+        max-width: 14dvh;
+    }
+
+    .qty {
+        width: 12dvh;
+        background-color: transparent;
+        border-color: transparent;
+    }
+
+    .pzs {
+        width: 10dvh;
+    }
+
+    .discount {
+        width: 5dvh;
+        background-color: transparent !important;
+    }
+
+    .discount-input {
+        max-width: 9dvh;
+    }
+
+    .select-discount {
+        max-width: 5dvh;
+    }
+
+    .panel {
+        border-radius: 18px;
+        border-top-left-radius: 0;
+        border-top-right-radius: 0;
+        border-bottom-right-radius: 0;
+        border: 1px solid #e4e6ef;
+        padding: 2dvh;
+        height: auto;
+        overflow-x: scroll;
+    }
+
+    .top-actions .btn {
+        background-color: white;
+        color: #60060F;
+        border-color: #E2BBB4
+    }
+
+    .employee-selector {
+        background-color: transparent;
+        border-color: transparent;
+    }
+
+    .employee-selector-2 {
+        width: 17dvh;
+        max-width: 17dvh !important;
+    }
+
+    .client-name {
+        margin-left: 1dvh;
+        border: 2px solid #E4E6EF;
+        border-radius: 5rem;
+        padding: 1dvh;
+    }
+
+    .client-name-container {
+        align-items: anchor-center;
+    }
+
+    .section-title {
+        font-size: 13px;
+        color: #888;
+        letter-spacing: .5px;
+        margin-top: 15px;
+        margin-bottom: 8px;
+    }
+
+    .table th {
+        font-size: 12px;
+        color: #888;
+        border-top: none;
+    }
+
+    .service-title {
+        max-width: 22dvh;
+        white-space: nowrap;
+        overflow: hidden;
+        text-overflow: ellipsis;
+    }
+
+    .unit-price-title {
+        max-width: 12dvh;
+    }
+
+    .employees-title {
+        max-width: 12dvh;
+    }
+
+    .table td {
+        vertical-align: middle;
+        white-space: break-word;
+    }
+
+    .table tbody tr:nth-child(even) {
+        background: #f7f9fc;
+    }
+
+    .summary-box {
+        background: #f7f9fc;
+        border-radius: 10px;
+        padding: 12px;
+        font-size: 13px;
+        border: 2px dashed #E4E6EF;
+    }
+
+    .total-box {
+        font-weight: 600;
+        font-size: 18px;
+    }
+
+    .btn-primary-custom {
+        background: #243b5e;
+        border: none;
+        border-radius: 25px;
+        padding: 12px;
+        color: #fff;
+        font-weight: 600;
+    }
+
+    .btn-primary-custom:hover {
+        background: #1b2e4a;
+        color: #fff;
+    }
+
+    .money {
+        font-weight: 600;
+    }
+
+    .money-danger {
+        color: #e55353;
+        font-weight: 600;
+    }
+
+    .money-success {
+        color: #2fbf71;
+        font-weight: 600;
+    }
+
+
+    .prices {
+        justify-content: center;
+        column-gap: 1rem;
+    }
+
+    .searching-container {
+        display: flex;
+        justify-content: flex-end;
+        gap: 10px;
+    }
+
+    .searching-results {
+        width: 30rem;
+        position: absolute;
+        z-index: 1;
+        max-height: 300px;
+        overflow-y: auto;
+        border: 2px solid #E4E6EF;
+    }
+
+    ::-webkit-scrollbar-track {
+        background: none;
+    }
+
+    .add-payment {
+        width: stretch;
+    }
+
+    .qty-payment {
+        max-width: 16dvh;
+    }
+
+    .qty-payment-2 {
+        width: 11dvh;
+    }
+
+    .reference-field {
+        min-width: 27dvh;
+    }
+
+    .reference-field-2 {
+        width: 18dvh;
+    }
+
+    .time-input {
+        width: 8rem;
+        background-color: transparent;
+        border-color: transparent;
+        text-align: center
+    }
+
+    .money-icon {
+        border: 2px solid;
+        width: fit-content;
+        border-radius: 50%;
+        margin-right: 1dvh;
+    }
+
+    .percent-qty-tip {
+        width: 10dvh !important;
+    }
+
+    .percent-tip {
+        width: 5dvh !important;
+    }
+
+    .billing-footer {
+        background-color: transparent;
+        border-top: 0;
+    }
+</style>
