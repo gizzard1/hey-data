cad76aa43961358c41bd5bd0c5332c27e7f8e282
gizzard1 <mauriciodds86@gmail.com>
2026-03-19 14:07:06 -0600

    Deleting deprecated data exported

--- resources/views/livewire/servicios/exportables/servicios.blade.php
+++ resources/views/livewire/servicios/exportables/servicios.blade.php
@@ -17,7 +17,6 @@
             <th>Precio público</th>
             <th>Precio descuento</th>
             <th>IVA</th>
-            <th>Puntos recompensa</th>
             <th>Duración</th>
             <th>Fecha de creación</th>
         </tr>
@@ -30,7 +29,6 @@
             <td>{{ $servicio->gross_price }}</td>
             <td>{{ $servicio->disccount_price }}</td>
             <td>{{ $servicio->iva }}</td>
-            <td>{{ $servicio->reward_points }}</td>
             <td>{{ $servicio->duration }}</td>
             <td>{{ $servicio->created_at }}</td>
         </tr>
